53d8057d2231853d15b9bd9e237dd510a536255d
Ricky Kirkendall <rkirkendall@sixgill.com>
2017-10-19 16:57:19 -0700

    removed unneeded dep

--- keras_cnn.py
+++ keras_cnn.py
@@ -4,7 +4,6 @@ from keras.datasets import mnist
 from keras.layers import Dense, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
-import matplotlib.pylab as plt
 import numpy
 
 batch_size = 128
